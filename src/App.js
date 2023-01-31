a02d6d7a973364ab6a6b86e6f319862ed07fe1cd
David EGEA <contactdavidegea@gmail.com>
2023-01-31 23:16:58 +0100

    ✨ F01 : LOGIN > MaJ LoginPage

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import { useState } from "react";
-import Loginform from "./components/Loginform"
+import "../src/App.css"
 import LoginPage from "./components/pages/LoginPage";
 
 function App () {
-  // state (état, données)
-  const [login, setLogin] = useState("");
-
-  // comportements
-
-  const handleAdd = (loginAAjouter) => {
-    // 1. copie du state
-    const loginCopy= [...login];
-
-    // 2. manipulation sur la copie du state
-    loginCopy.push({ loginAAjouter });
-
-    // 3. modifier le state avec le setter
-    setLogin(loginCopy);
-  }
-
-  // affichage (render)
-  return (
-    <div>
-      <LoginPage />
-      <h1>Bienvenue chez nous !</h1>
-      <h2>Connectez-vous</h2>
-      <Loginform handleAdd={handleAdd} />
-    </div>
-  );  
+  return <LoginPage />
 }
 
 export default App;
