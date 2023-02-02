6d5e77232b7625d9621e3916fbd5e0279c010ef2
David EGEA <contactdavidegea@gmail.com>
2023-02-02 22:43:39 +0100

    ✨ F01: LOGIN > Add Title and Form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,3 +1,30 @@
+import { useState } from "react";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>
+  // state
+  const [login, setLogin] = useState("");
+
+  // comportements
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${login}`);
+    setLogin("");
+  }
+
+  const handleChange = (event) => { 
+    setLogin(event.target.value);
+  }
+
+  // affichage
+  return (
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input value={login} type="text" placeholder="Entrez votre prénom..." required onChange={handleChange}/>
+        <button>Accéder à votre espace</button>
+      </form>
+    </div>
+    )
   }
