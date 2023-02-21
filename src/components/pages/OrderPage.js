ac1fc7c1a1a8bb1c521bf7437d67ed1d17cc33ba
David EGEA <contactdavidegea@gmail.com>
2023-02-21 23:29:08 +0100

    ✨ F02: OrderPage > Create page

---
+++ src/components/pages/OrderPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function OrderPage() {
+  return (
+    <h1>Bonjour</h1>
+  )
+}
--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,7 +1,18 @@
 import LoginForm from "./LoginForm"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import OrderPage from "../OrderPage";
 
 export default function LoginPage() {
 
   // affichage
-  return <LoginForm />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/order" element={<OrderPage />} />
+        {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
+        <Route path="*" element={<LoginForm />} />
+      </Routes>
+    </BrowserRouter>
+  )
   }
