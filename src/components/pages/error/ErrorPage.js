9226905f547c9147cf799eab2a7e650807fb6ffd
David EGEA <contactdavidegea@gmail.com>
2023-02-24 23:00:47 +0100

    ✨ F02: LOGIN > add redirection from/to ErrorPage

--- src/components/pages/error/ErrorPage.js
+++ src/components/pages/error/ErrorPage.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function ErrorPage() {
   return (
     <div>
         <h1>Erreur</h1>
         <br />
-        <NavLink to="/">
-            <button>Retour à la page d'accueil</button>
-        </NavLink>
+        <Link to="/">
+            <button>Retourner vers la page d'accueil</button>
+        </Link>
     </div>
   )
 }
